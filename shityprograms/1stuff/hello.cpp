a1f0e2cb8ec75146b141f2459c1dc6e7cdb39270
themanhimeslf <aulua003@osloskolen.no>
2025-04-02 09:49:38 +0200

    Suffering across the ages, .cpp .asm .c not working, library issues, and for asm no idea

---
+++ shityprograms/1stuff/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // input-output library
+
+using namespace std;  // standard namespace
+
+int main() {
+    cout << "Hello, World!" << endl;  // Print Hello World
+    return 0;  // Return 0 
+}
