bd2452736f6cfa244f03853994ddb8d7413f05ad
themanhimeslf <aulua003@osloskolen.no>
2025-05-19 22:21:42 +0200

    file location

--- FlaskAPP.py
+++ FlaskAPP.py
@@ -31,6 +31,7 @@ def Mod():
 @app.route("/about")
 def About():
     return render_template("about.html")
+print("or localhost 8080")
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
