af128862c37ab019559d4e1ec77af3acc05b33a9
themanhimeslf <aulua003@osloskolen.no>
2025-06-02 13:55:08 +0200

    Templates, flask etcc

--- FlaskAPP.py
+++ FlaskAPP.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session, url_for, request
 from waitress import serve
 from query import get_player_count
-
+from db_connect import connect_to_database
+import hashlib
 import logging
 
 
-# Set up logging
+# Set for up logging
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('waitress').setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
@@ -33,9 +34,38 @@ def Mod():
 def About():
     return render_template("About.html")
 
-@app.route("/adminlogin")
+@app.route("/adminlogin", methods=["GET", "POST"])
 def adminlogin():
+    if request.method == "POST":
+        admin_id = request.form["adminid"]
+        password = request.form["password"]
+
+        # hasher passordet
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+        # sjekk db
+        conn = connect_to_database()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM ADMIN WHERE AdminID = %s AND AdminPS = %s", (admin_id, hashed_password))
+        admin = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if admin:
+            # Ssave sesion redirect?
+            return redirect("/adminpage")  # adminpge
+        else:
+            return "Login failed", 401
+
     return render_template("adminlogin.html")
+
+@app.route("/adminpage")
+def admin_page():
+    if admin:
+        session['admin_logged_in'] = True
+        session['admin_id'] = admin['AdminID']
+        return redirect("/adminpage")
+
 
 
 if __name__ == '__main__':
